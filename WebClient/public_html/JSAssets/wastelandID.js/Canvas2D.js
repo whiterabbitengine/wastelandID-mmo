5d33b51d998a89ed6b4474ba279b1f876b94f44f
whiterabbit <whiterabbit@dev-one.localhost>
2014-02-24 21:57:27 -0600

    removed some files and created some more

--- WebClient/public_html/JSAssets/wastelandID.js/Canvas2D.js
+++ WebClient/public_html/JSAssets/wastelandID.js/Canvas2D.js
@@ -1,7 +1,11 @@
-<script>
-// declare the two variables at once using a comma
-var canvas = document.getElementById("c"),
-    context = canvas.getContext("2d");
+function resizeCanvas() {
+            c.width = (window.innerWidth)*0.98;
+            c.height = (window.innerHeight)*0.98;
 
-// now you're ready to draw
-</script>
+            /**
+             * Your drawings need to be inside this function otherwise they will be reset when 
+             * you resize the browser window and the canvas goes will be cleared.
+             */
+       	ctx.fillStyle="#FF0000";
+	ctx.fillRect(0,0,150,75);
+    }
--- WebClient/public_html/index.php
+++ WebClient/public_html/index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link href="css/main-style-1.css" rel="stylesheet" type="text/localization/english_US/css" media="screen" />
-	<script src="JSAssets/jquery.min.js"></script>
-	<script src="JSAssets/wastelandID/Canvas2D.js"></script>
+<!-- STYLES START -->
+<link href="css/main-style-1.css" rel="stylesheet" type="text/localization/english_US/css" media="screen" />
+<link href='http://fonts.googleapis.com/css?family=Jura' rel='stylesheet' type='text/css'>
+<!-- STYLES END ---->
+<script src="JSAssets/jquery.min.js"></script>
+<script src="JSAssets/fancywebsocket.js"></script>
+<script src="JSAssets/wastelandID.js/Canvas2D.js"></script>
 </head>
 <body>
-<canvas id="c"></canvas>
+
+<canvas id="myCanvas" width="200" height="100" style="border:1px solid #c3c3c3; position: fixed; top: 0; bottom: 0; left: 0; right: 0; margin: auto;">
+Your browser does not support the HTML5 canvas tag.
+</canvas>
+
+<script>
+
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+
+window.addEventListener('resize', resizeCanvas, false);
+resizeCanvas();
+
+</script>
+
 </body>
 </html>
 
