c25a7e3868f162a6d88e616b14c0d9e6422955b3
whiterabbit <whiterabbit@dev-one.localhost>
2014-02-24 21:06:58 -0600

    deprecated pixi.js library in lieu of using canvas2D and self baked system

---
+++ WebClient/public_html/JSAssets/wastelandID.js/Canvas2D.js
@@ -0,0 +1,7 @@
+<script>
+// declare the two variables at once using a comma
+var canvas = document.getElementById("c"),
+    context = canvas.getContext("2d");
+
+// now you're ready to draw
+</script>
---
+++ WebClient/public_html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="css/main-style-1.css" rel="stylesheet" type="text/localization/english_US/css" media="screen" />
+	<script src="JSAssets/jquery.min.js"></script>
+	<script src="JSAssets/wastelandID/Canvas2D.js"></script>
+</head>
+<body>
+<canvas id="c"></canvas>
+</body>
+</html>
+
